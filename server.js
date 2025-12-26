feb1229d7c637143dce945e2236f6d247419c276
harshpreetsolutions1313 <harshpreetsolutions1313@gmail.com>
2025-12-26 12:12:44 +0530

    server change

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // REQUIRED on Vercel for preflight
-app.options('*', cors(corsOptions));
+// app.options('/*', cors(corsOptions));
 
 app.use(express.json());
 
